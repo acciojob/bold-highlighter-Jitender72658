113a261b25a49981f3bf91d0c04bf732792b4713
Jitender Kumar <111197506+Jitender72658@users.noreply.github.com>
2023-07-24 15:51:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 function highlight() {
-    //Write your code here
+     let boldTags = document.getElementsByTagName('strong');
+     for (let i = 0; i < boldTags.length; i++) {
+     boldTags[i].style.color = 'green'; // Change the color to green
+}
+ }
 
-
+ 
+ 
+ function return_normal() {
+     let boldTags = document.getElementsByTagName('strong');
+     for (let i = 0; i < boldTags.length; i++) {
+   boldTags[i].style.color = 'black';
+     
+ }
 }
 
-
-function return_normal() {
-    //Write your code here
-
-    
-}
